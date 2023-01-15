a3be2dad9079cc415d61aabd7bde33f4bcd34f57
ghostpunk25 <105439703+ghostpunk25@users.noreply.github.com>
2023-01-15 10:57:54 +0200

    add

--- src/componets/Box/Box.jsx
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-import { color, space, typography, layout, flexbox, background, position, grid, border } from 'styled-system';
-
-export const Box = styled('div')(color, space, typography, layout, flexbox, background, position, grid, border)
-
--- src/componets/Statistics/Statistics.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Status = styled.span`
-width: 20px;
-height: 20px;
-background:${p => p.isOnline ? p => p.theme.colors.green : p => p.theme.colors.red};
-border-radius: ${p => p.theme.radii.round};
-`
